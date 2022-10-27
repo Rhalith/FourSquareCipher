4dd056462dd65f9666d297f02ccf11311de91f8c
Rhalith <81980850+Rhalith@users.noreply.github.com>
2022-10-27 15:11:56 +0200

    Added test methods.

---
+++ src/test/java/pl/polsl/nuhyigitakman/modeltest/CoderTest.java
@@ -0,0 +1,15 @@
+package pl.polsl.nuhyigitakman.modeltest;
+
+import org.junit.jupiter.api.*;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled("Disabled until method bodies get filled")
+public class CoderTest {
+
+    @Test
+    public void testEncodeText(){
+
+    }
+}
---
+++ src/test/java/pl/polsl/nuhyigitakman/modeltest/MatricesTest.java
@@ -0,0 +1,36 @@
+package pl.polsl.nuhyigitakman.modeltest;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled("Disabled until method bodies get filled.")
+public class MatricesTest {
+    @Test
+    public void testGetPlot(){
+
+    }
+    @Test
+    public void testGetPlotTwo(){
+
+    }
+    @Test
+    public void testGetPlotThree(){
+
+    }
+    @Test
+    public void testGetRemove(){
+
+    }
+    @Test
+    public void testPlotAlphabets(){
+
+    }
+    @Test
+    public void testPlotKey(){
+
+    }
+    @Test
+    public void testRandomizeKey(){
+
+    }
+}
