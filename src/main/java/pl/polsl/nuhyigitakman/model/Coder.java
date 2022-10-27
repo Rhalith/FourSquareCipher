3b7596804d1d7824a6e09fe2693e4977fc7a51f0
Rhalith <81980850+Rhalith@users.noreply.github.com>
2022-10-27 02:33:12 +0200

    deleted some comments lines

--- src/main/java/pl/polsl/nuhyigitakman/model/Coder.java
+++ src/main/java/pl/polsl/nuhyigitakman/model/Coder.java
@@ -1,6 +1,5 @@
 package pl.polsl.nuhyigitakman.model;
 
-import java.util.Arrays;
 
 /**
  *
@@ -50,13 +49,10 @@ public class Coder {
         int cursor = 0;
         for (int i = 0; i < pairsOfEncryptedText.length; i++) {
             pairsOfEncryptedText[i] = "" + encryptedText.charAt(cursor) + encryptedText.charAt(cursor + 1);
-            // step by two
             cursor = cursor + 2;
         }
 
-        System.out.println("Pairs: " + Arrays.toString(pairsOfEncryptedText));
 
-        // O(nk)
         for (String s : pairsOfEncryptedText) {
             int column_a = 0;
             int row_a = 0;
@@ -69,27 +65,21 @@ public class Coder {
 
             // find the pairs on key plot 1 NormalAlphabets
             for (int j = 0; j < plot.length; j++) {
-                // find the first pair to from the normal alphabets
                 if (pairs[0] == plot1[j]) {
-                    // the plot is 5 by 25 and we need to find the row, we can convert it to 5x5 (divide 5)
                     row_a = (j / 5) * 5;
-                    // We need to find the column by seeing the remainder, therefore we mod it by 5
                     column_a = j % 5;
                 }
 
-                // find the second pair to from the normal alphabets
                 if (pairs[1] == plot2[j]) {
                     row_b = (j / 5) * 5;
                     column_b = j % 5;
                 }
             }
 
-            // we need to reverse the column and the row in the two key plot (ciphered plot)
             decryptedText += plot[row_a + column_b]; // find in the key plot 1 Q1
             decryptedText += plot[row_b + column_a];  // find in the key plot 2 Q4
         }
 
-        // show the encrypted text
         return decryptedText;
     }
     /**
@@ -109,41 +99,30 @@ public class Coder {
         int cursor = 0;
         for (int i = 0; i < pairsOfEncryptText.length; i++) {
             pairsOfEncryptText[i] = "" + plainText.charAt(cursor) + plainText.charAt(cursor + 1);
-            // step by two
             cursor = cursor + 2;
         }
 
-        // show the pairs
-        System.out.println("Pairs: " + Arrays.toString(pairsOfEncryptText));
-
-        // O(nk)
         for (String s : pairsOfEncryptText) {
             int column_a = 0;
             int row_a = 0;
             int column_b = 0;
             int row_b = 0;
 
-            // get the pairs
             pairs[0] = s.charAt(0);
             pairs[1] = s.charAt(1);
 
             // find the pairs on key fixed alphabet
             for (int j = 0; j < plot.length; j++) {
-                // find the first pair to from the fixed alphabets
                 if (pairs[0] == plot[j]) {
-                    // the plot is 5 by 25 and we need to find the row, we can convert it to 5x5 (divide 5)
                     row_a = (j / 5) * 5;
-                    // to find the column we need to see the remainder, we (mod) it by 5 (j % 5)
                     column_a = j % 5;
                 }
 
-                // find the second pair to from the fixed alphabets
                 if (pairs[1] == plot[j]) {
                     row_b = (j / 5) * 5;
                     column_b = j % 5;
                 }
             }
-            // we need to reverse the column and the row in the two key plot (ciphered plot)
             encryptedText += plot1[row_a + column_b]; // find in the key plot 1 Q2
             encryptedText += plot2[row_b + column_a];  // find in the key plot 2 Q3
         }
