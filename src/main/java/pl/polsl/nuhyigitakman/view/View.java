eaab2dea68a799790a62376068849beca2f64113
Rhalith <81980850+Rhalith@users.noreply.github.com>
2022-11-10 15:22:32 +0100

    added basic of gui

--- src/main/java/pl/polsl/nuhyigitakman/view/View.java
+++ src/main/java/pl/polsl/nuhyigitakman/view/View.java
@@ -2,6 +2,9 @@ package pl.polsl.nuhyigitakman.view;
 
 import pl.polsl.nuhyigitakman.controller.Controller;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -35,67 +38,68 @@ public class View {
      * Main run method for program.
      */
     public void run(){
-        /**
-         * User encoding choice
-         */
-        String userChoice = new String();
-        /**
-         * User's key choice(random or manual)
-         */
-        String keyChoice = new String();
-        /**
-         * User's key input
-         */
-        String keyInput = new String();
-        /**
-         * User's input for encryption or decryption
-         */
-        String userText = new String();
-        /**
-         * Result of encoding
-         */
-        String output = new String();
-        /**
-         * Question text for getting input from user
-         */
-        String questionText = new String();
-        /**
-         * Warning text for user to get right input.
-         */
-        String warningText = new String();
-
-
-        questionText = "Encrypt for 'E', Decrypt for 'D'";
-        warningText = "Please make your choice between 'E' or 'D'!";
-        userChoice = getInput(scanner, questionText, warningText, "E", "D");
-
-        System.out.println();
-
-        questionText = "If you want to enter your own key enter 'M'\nIf you want to let computer to decides key randomly enter 'R'";
-        warningText = "Please make your choice between 'M' or 'R'!";
-
-        keyChoice = getInput(scanner, questionText, warningText, "M", "R");
-
-        if(keyChoice.equals("M")){
-            questionText = "Please enter key 1:";
-            keyInput = getInput(scanner, questionText, true);
-
-            controller.updateController(2, keyInput);
-            questionText = "Please enter key 2:";
-            keyInput = getInput(scanner, questionText, true);
-            controller.updateController(3, keyInput);
-        }
-        else{
-            controller.updateController(2, "");
-            controller.updateController(3, "");
-        }
-        controller.updateController(1,"");
-        questionText = "Please enter your text: ";
-        userText = getInput(scanner, questionText, false);
-
-        output = controller.updateController(userChoice, userText);
-
-        getOutput(userText, output, userChoice);
+        new EncryptOrDecrypt();
+//        /**
+//         * User encoding choice
+//         */
+//        String userChoice = new String();
+//        /**
+//         * User's key choice(random or manual)
+//         */
+//        String keyChoice = new String();
+//        /**
+//         * User's key input
+//         */
+//        String keyInput = new String();
+//        /**
+//         * User's input for encryption or decryption
+//         */
+//        String userText = new String();
+//        /**
+//         * Result of encoding
+//         */
+//        String output = new String();
+//        /**
+//         * Question text for getting input from user
+//         */
+//        String questionText = new String();
+//        /**
+//         * Warning text for user to get right input.
+//         */
+//        String warningText = new String();
+//
+//
+//        questionText = "Encrypt for 'E', Decrypt for 'D'";
+//        warningText = "Please make your choice between 'E' or 'D'!";
+//        userChoice = getInput(scanner, questionText, warningText, "E", "D");
+//
+//        System.out.println();
+//
+//        questionText = "If you want to enter your own key enter 'M'\nIf you want to let computer to decides key randomly enter 'R'";
+//        warningText = "Please make your choice between 'M' or 'R'!";
+//
+//        keyChoice = getInput(scanner, questionText, warningText, "M", "R");
+//
+//        if(keyChoice.equals("M")){
+//            questionText = "Please enter key 1:";
+//            keyInput = getInput(scanner, questionText, true);
+//
+//            controller.updateController(2, keyInput);
+//            questionText = "Please enter key 2:";
+//            keyInput = getInput(scanner, questionText, true);
+//            controller.updateController(3, keyInput);
+//        }
+//        else{
+//            controller.updateController(2, "");
+//            controller.updateController(3, "");
+//        }
+//        controller.updateController(1,"");
+//        questionText = "Please enter your text: ";
+//        userText = getInput(scanner, questionText, false);
+//
+//        output = controller.updateController(userChoice, userText);
+//
+//        getOutput(userText, output, userChoice);
     }
 
 
@@ -183,5 +187,104 @@ public class View {
             System.out.println("Your text was: "+input);
             System.out.println("Your decrypted text is:"+output);
         }
+    }
+}
+class EncryptOrDecrypt{
+    EncryptOrDecrypt(){
+        JFrame f = new JFrame();
+        JButton e = new JButton("Encryption");
+        JButton d = new JButton("Decryption");
+        e.setBounds(50,50,100,40);
+        d.setBounds(250,50,100,40);
+        f.add(e);
+        f.add(d);
+        f.setSize(400,150);
+
+        f.setLayout(null);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        e.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                new RandomOrManual(f);
+            }
+        });
+        d.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                new RandomOrManual(f);
+            }
+        });
+    }
+}
+class RandomOrManual {
+    RandomOrManual(JFrame f){
+        JFrame m = new JFrame();
+        JButton manual = new JButton("Manual");
+        JButton random = new JButton("Random");
+        manual.setBounds(50,50,100,40);
+        random.setBounds(250,50,100,40);
+        m.add(manual);
+        m.add(random);
+        m.setSize(400,400);
+        f.dispose();
+        m.setLayout(null);
+        m.setVisible(true);
+        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        manual.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                //new KeyInputMenu(m);
+                //will be used after filled keyinputmenu properly
+            }
+        });
+        random.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                new TextInputMenu(m);
+            }
+        });
+    }
+}
+
+class KeyInputMenu{
+    KeyInputMenu(JFrame f){
+        JFrame m = new JFrame();
+        JTextField text = new JTextField();
+        JButton enter = new JButton("");
+        enter.setBounds(50,50,100,40);
+        text.setBounds(50,100,100,40);
+        m.add(enter);
+        m.add(text);
+        m.setSize(400,400);
+        f.dispose();
+        m.setLayout(null);
+        m.setVisible(true);
+        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        enter.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                //will set key to plot
+            }
+        });
+
+    }
+}
+
+class TextInputMenu{
+    TextInputMenu(JFrame f){
+        JFrame m = new JFrame();
+        JTextField text = new JTextField();
+        JButton enter = new JButton("Enter");
+        enter.setBounds(50,50,100,40);
+        text.setBounds(50,100,100,40);
+        m.add(enter);
+        m.add(text);
+        m.setSize(400,400);
+        f.dispose();
+        m.setLayout(null);
+        m.setVisible(true);
+        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        enter.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                JOptionPane.showMessageDialog(null, "Your crypted text is: nullforprototype");
+                m.dispose();
+            }
+        });
     }
 }
